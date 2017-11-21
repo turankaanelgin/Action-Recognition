2e64e4d280f6a22d13c48c33e974070e60aa0af9
Aya <ayaabdelsalam91@github.com>
2017-11-20 21:46:42 -0500

    Shuffle data

--- code/cnn_lstm_rawframe/cnn_lstm_model.py
+++ code/cnn_lstm_rawframe/cnn_lstm_model.py
@@ -10,11 +10,16 @@ import numpy as np
 def  getAUC (true , output):
     correct = 0
     for i in range(true.shape[0]):
-        #print true[i] , output[i], "true", np.argmax(true[i])+1 ,"predicted" , np.argmax(output[i])+1
+        #print( true[i] , output[i], "true", np.argmax(true[i])+1 ,"predicted" , np.argmax(output[i])+1)
         if(np.argmax(true[i]) ==    np.argmax(output[i])):
             correct+=1
     return float(correct)/true.shape[0]
 
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
 
 CATEGORIES = ["walking", "jogging", "running", "boxing", "handwaving", "handclapping"]
 DATASET_DIR = "../../data"
@@ -44,7 +49,7 @@ pool2_fmaps = conv2_fmaps
 n_neurons = 150
 n_outputs = 6
 n_layers=3
-
+learning_rate=0.1
 
 with tf.name_scope("inputs"):
     X = tf.placeholder(tf.float32, shape=[None,height,width,n_inputs], name="X")
@@ -55,21 +60,22 @@ with tf.name_scope("inputs"):
 conv1 = tf.layers.conv2d(X, filters=conv1_fmaps, kernel_size=conv1_ksize,
                           padding=conv1_pad,
                          activation=tf.nn.relu, name="conv1")
-# print("conv1",conv1)
+print("conv1",conv1)
 
 with tf.name_scope("pool1"):
-    print(pool1_ksize , pool1_stride)
+    # print(pool1_ksize , pool1_stride)
     pool1 = tf.nn.avg_pool(conv1, ksize=pool1_ksize, strides=pool1_stride, padding="VALID")
    
-# print("pool1",pool1)
+print("pool1",pool1)
 
 conv2 = tf.layers.conv2d(pool1, filters=conv2_fmaps, kernel_size=conv2_ksize,padding=conv2_pad,
                          activation=tf.nn.relu, name="conv2")
-# print("conv2",conv2)
+print("conv2",conv2)
 
 with tf.name_scope("pool2"):
     pool2 = tf.nn.max_pool(conv2, ksize=pool2_ksize, strides=pool2_stride, padding="VALID")
     pool2_flat = tf.reshape(pool2, shape=[-1, pool2_fmaps  , 20 * 15])
+    print("pool2_flat" , pool2_flat)
 
 
 
@@ -77,27 +83,27 @@ with tf.name_scope("lstm_cell"):
     lstm_cells = [tf.contrib.rnn.BasicLSTMCell(num_units=n_neurons)
               for layer in range(n_layers)]
     
-    # print("lstm_cells",lstm_cells)
+    print("lstm_cells",lstm_cells)
 
 with tf.name_scope("multi_cell"):
     multi_cell = tf.contrib.rnn.OutputProjectionWrapper(
         tf.contrib.rnn.MultiRNNCell(lstm_cells),output_size=n_outputs)
-    # print("multi_cell",multi_cell)
+    print("multi_cell",multi_cell)
 
 with tf.name_scope("outputs"):
     outputs, states = tf.nn.dynamic_rnn(multi_cell, pool2_flat, dtype=tf.float32)
-    # print("outputs",outputs)
+    print("outputs",outputs)
 
 
 
 with tf.name_scope("logits"):
     logits=outputs[:,-1,:]
-    # print("logits",logits)
+    print("logits",logits)
 
 with tf.name_scope("train"):
     xentropy = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=logits)
     loss = tf.reduce_mean(xentropy, name="loss")
-    optimizer = tf.train.AdamOptimizer()
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
     training_op = optimizer.minimize(loss)
 
 
@@ -125,11 +131,14 @@ train_labels=train_labels.reshape((train_labels.shape[0] ,train_labels.shape[1] 
 test=test.reshape((test.shape[0] ,test.shape[2], test.shape[3] , test.shape[1] ))
 test_labels=test_labels.reshape((test_labels.shape[0] ,test_labels.shape[1] ))
 
+# for i in range(len(train_labels)):
+#     print(train_labels[i])
+
 print(len(train))
 with tf.Session() as sess:
     init.run()
     for epoch in range(n_epochs):
-        
+        train  ,  train_labels= unison_shuffled_copies(train, train_labels)
         for i in range(0 , len(train)-100,100):
             # print(epoch, i)
             X_batch =train[i:i+100] 
@@ -138,6 +147,13 @@ with tf.Session() as sess:
             
             sess.run(training_op, feed_dict={X: X_batch, y: Y_batch})
 
+            sess.run(training_op, feed_dict={X: X_batch, y: Y_batch})
+            pred_batch = preds.eval(feed_dict={X: X_batch, y: Y_batch})
+            xentropy_batch = loss.eval(feed_dict={X: X_batch, y: Y_batch})
+            acc_batch = getAUC(Y_batch , pred_batch)
+            print(i, "Batch accuracy:", acc_batch, "xentropy_batch" ,  xentropy_batch)
+
+
         
         pred_train = preds.eval(feed_dict={X: train, y: train_labels})
         acc_train = getAUC(train_labels , pred_train)
@@ -145,5 +161,4 @@ with tf.Session() as sess:
         acc_test = getAUC(test_labels , pred_test)
 
         print(epoch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
-
     saver.save(sess, open(os.path.join(DATASET_DIR, "cnn_raw_frame")))
